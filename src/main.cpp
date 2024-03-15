dd7f08e7f73be9be99abfb4b9a39537052f7af8f
Hussain7252 <hussainkanc52@gmail.com>
2024-03-15 12:19:58 -0400

    task3push

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,9 @@ int main(){
 
     // Display window
     namedWindow("Video Frame", WINDOW_NORMAL);
-    
+
+    // Trivial Variables
+    bool messageDisplay = false;
     while (true)
     {
         int key = waitKey(1);
@@ -55,13 +57,47 @@ int main(){
                 imwrite(calibimg+"/"+cimgname,frame);
                 corner_list.push_back(corner_set);
                 point_list.push_back(point_set);
+                messageDisplay = false;
+                if(corner_list.size()>4 && !messageDisplay){
+                    cout<<"Press 'C' to calibrate"<<endl;
+                    messageDisplay = true;
+                }
             }
+        }
+        if (corner_list.size()>4 && (key == 'C'||key == 'c')){
+            Mat camera_matrix = Mat::eye(3, 3, CV_64FC1);
+            camera_matrix.at<double>(0, 2) = frame.cols / 2.0;
+            camera_matrix.at<double>(1, 2) = frame.rows / 2.0;
+            //initializing the distortion coefficient
+            Mat dist_coeff = Mat::zeros(8,1,CV_64FC1);
+            Mat rot, trans;
+            cout << "Camera matrix before Calibration: " << endl << camera_matrix << endl;
+            cout << "Distortion coefficients before Calibration: " << dist_coeff << endl;
+
+            double rms_error = calibrateCamera(point_list, corner_list, frame.size(), camera_matrix, dist_coeff, rot, trans,CALIB_FIX_ASPECT_RATIO);
+
+            cout << "Camera matrix: " << endl << camera_matrix << endl;
+            cout << "Distortion coefficients: " << dist_coeff << endl;
+            cout << "Reprojection error: " << rms_error << endl;
+
+            // Saving calibration
+            FileStorage fs("/home/hussain/computer_vision/CourseWork/Project4/Calibration_file/calibration.yml", FileStorage::WRITE);
+            fs << "camera_matrix" << camera_matrix;
+            fs << "distortion_coefficients" << dist_coeff;
+            fs.release();
+            cout << "Calibration saved to 'calibration.yml'" << endl;
+            cout << "If you want to calibrate with more calibration images press 'S' and follow the procedure"<<endl;
+            cout << "Or else press 'ESC' to exit"<<endl;
+
+        }else if (corner_list.size() < 5 && !messageDisplay) {
+        cout << "Please Take at least 5 images to calibrate by pressing 'S'. Images taken: " << corner_list.size() << endl;
+        messageDisplay = true;
         }
 
         if(key == 27){
             break;
         }
     }
-    
+
     return 0;
 }
